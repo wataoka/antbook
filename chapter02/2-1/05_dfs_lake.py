21129253a3751fb482f0a81a3b26ccff476c8168
Koki Wataoka <contact@wataoka.com>
2019-06-25 22:09:45 +0900

    add: lake

--- chapter02/2-1/05_dfs_lake.py
+++ chapter02/2-1/05_dfs_lake.py
@@ -1,4 +1,4 @@
-n = int(input())
+n, m = map(int, input().split())
 field = []
 for i in range(n):
     field.append(list(input()))
@@ -10,9 +10,19 @@ def dfs(x, y):
         for dy in [-1, 0, 1]: 
             if not((dx, dy) == (0, 0)):
                 next_x, next_y = x+dx, y+dy
-                if (0 <= next_x and next_x < n) and (0 <= next_y and next_y < n):
+                if (0 <= next_x and next_x < n) and (0 <= next_y and next_y < m):
                     if (field[next_x][next_y] == 'W'):
                         dfs(next_x, next_y)
 
 def solve():
     res = 0
+    for i in range(n):
+        for j in range(m):
+            if (field[i][j] == 'W'):
+                dfs(i, j)
+                res += 1
+
+    print(res)
+
+if __name__ == "__main__":
+    solve()
