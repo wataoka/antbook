85faa61a41507c6d2a410f16c45efe8099da47f3
Koki Wataoka <contact@wataoka.com>
2019-10-07 01:40:25 +0900

    add: napsack

---
+++ chapter02/2-3/01_knapsack.py
@@ -0,0 +1,20 @@
+N = int(input())
+w = []
+v = []
+for i in range(N):
+    w_i, v_i = map(int, input().split())
+    w.append(w_i)
+    v.append(v_i)
+W = int(input())
+
+# dp[i][j]は重さjまで入れていい状況においてi番目までの最適なvalueを保証している。
+dp = [[0]*(W+1) for i in range(N)]
+
+for i in range(N-1):
+    for j in range(W+1):
+        if w[i] > j:
+            dp[i+1][j] = dp[i][j]
+        else:
+            dp[i+1][j] = max(dp[i][j], dp[i][j-w[i]]+v[i])
+
+print(dp[-1][-1])
